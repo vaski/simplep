cec53b4a61f497f8b33bed7cb95d5b63663910ed
Vadim Masakovski <vmasakovski@cloudpassage.com>
2017-09-04 18:25:29 +0300

    initial commit

---
+++ src/main/java/simplep/Account.java
@@ -0,0 +1,61 @@
+package simplep;
+
+import java.util.Date;
+
+public class Account {
+	private String name;
+	private String type;
+	private long id;
+	private double balance;
+	private Date createdAt;
+	private Date expirationDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+}
